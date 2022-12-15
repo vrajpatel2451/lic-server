a6bc561724e6b0141c6f963b9403af7e6b60325b
Vraj Patel <iampatelvraj@gmail.com>
2022-12-15 11:35:27 +0530

    added

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -75,20 +75,21 @@ export function roleMaker (){
 
 export const uploadUserFile = (req,res,next) => {
   const response = new ResponseWrapper(res);
-  // new FileOperations().upload((req,file,cb)=>{
-  //   if(file.mimetype==='image/png'||file.mimetype==='image/jpg' || file.mimetype === 'application/octet-stream'){
-  //     cb(null,true)
-  //   }else{
-  //     console.log('not suported',file);
-  //     cb(null,false)
-  //   }
-  // },
-  // {
-  //   fileSize:1024*1024
-  // }
-  // )
-  console.log(process.env.BUCKET_SECRET_ACCESS_KEY);
-  mediaUploader.single('userPhoto')(req,res,(err)=>{
+  new FileOperations().upload((req,file,cb)=>{
+    if(file.mimetype==='image/png'||file.mimetype==='image/jpg' || file.mimetype === 'application/octet-stream'){
+      cb(null,true)
+    }else{
+      console.log('not suported',file);
+      cb(null,false)
+    }
+  },
+  {
+    fileSize:1024*1024
+  }
+  )
+  // console.log(process.env.BUCKET_SECRET_ACCESS_KEY);
+  // mediaUploader.single('userPhoto')
+  (req,res,(err)=>{
     console.log('error here',err);
     if(err) {
       return response.internalServerError()
